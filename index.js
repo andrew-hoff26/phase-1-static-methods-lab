4c47e1ce59df319b294d2aa66ee239daf9ada322
andrew <andrew_hoffman262@outlook.com>
2023-11-24 22:31:57 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+  static titleize(string) {
+    const words = string.split(' ');
+    const nonCapitalizedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    
+    const CapitalizedWords = words.map((word, index) => {
+     if (index === 0 || !nonCapitalizedWords.includes(word)) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    } else {
+      return word;
+    }
+  })
+return CapitalizedWords.join(' ');
+  
+  }
 }
